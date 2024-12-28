d8324c044106fc12c8985bad034de9c9eeb27f85
Cindy Sintiya <211110347@students.mikroskil.ac.id>
2024-12-28 17:54:40 +0700

    update ui and upload model
    - (bug fixed) camera capture after upload image
    - (add) model from google colab, bad accuracy n loss
    - (add) model prediction and discrete

--- app.py
+++ app.py
@@ -1,103 +1,185 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import streamlit.components.v1 as components
-from PIL import Image
-from preprocess import *
 
-# page config
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
+# All preprocessing function
+from preprocess import preprocess_input
+
+# Page configuration
 st.set_page_config(
-  page_title="Babanana",
+  page_title="Bababa-babanana",
   page_icon="🍌",
   menu_items={
-    'About': "# Tugas Deep Learning"
+    "About": "# Tugas Deep Learning"
   }
 )
 
-# main page
-st.title('Banana Ripeness Detector')
-@st.dialog("Take a Photo")
-def capture():
-  st.write("Take a photo of a banana. Make sure it is xxx")
-  picture = st.camera_input("Take a picture")
-  if picture:
-    st.session_state.image = picture
-    st.rerun()
+model, model2 = None, None
 
-input_container = st.container(border=True)
-input_image = input_container.file_uploader(
-  label='Upload banana image',
-  type=['jpg','jpeg','png'],
-)
-if (input_image):
-  st.session_state.image =  Image.open(input_image)
-  input_image = None
+def load_model():
+  global model, model2
+  if model is None:
+    with st.spinner("Loading model for the first time"):
+      import tensorflow as tf
+      from tensorflow.keras import Model
 
-input_container.markdown('''<p style="text-align: center;">OR</p>''', unsafe_allow_html=True)
-take_photo = input_container.button('Take a Photo', use_container_width=True)
-if take_photo:
-  capture()
+      model = tf.keras.models.load_model('model/my_model.keras')
+      model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+      
+      # Redefine model to output right after the first hidden layer
+      ixs = [2,4,6]
+      model2 = Model(inputs=model.inputs, outputs=[model.layers[i].output for i in ixs])  
+  return model
 
-def predict(image: Image):
-  list_img = preprocess_input(st.session_state.image)
-  try:
-    yellow_mask, green_mask, img_edge = list_img[0]
-    combined_img = list_img[1]
-    x_predict = [combined_img]
-    x_predict = np.array(x_predict, dtype=float)
-    x_predict /= 255.0
+
+def process_prediction(uploaded_file):
+  next_predict = False
+  st.markdown("<h2 style='text-align: center;'>- Preprocessed Image -</h2>", unsafe_allow_html=True)
+  with st.spinner("Processing your file..."):
+    # Convert to image for preprocessing
+    img = Image.open(uploaded_file)
+    combined_image, img_clean, yellow_mask, green_mask, img_edge = preprocess_input(img)
     
-    prediction = 0.4
-    # comment code above when model is exported
-    # uncomment code below when model is exported
-    # prediction = model.predict(predict)
-    if (prediction < 0.5):
-      st.success('Pisang Mentah')
-    else:
-      st.warning('Pisang Matang')
-  except:
-    st.error('Bukan Pisang')
-  
-  with st.expander("See Details"):
-    col1, col2, = st.columns(2, gap='small')
-    col3, col4 = st.columns(2, gap='small')
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-      st.write('Original Image')
-      st.image(st.session_state.image, width=200)
+      st.image(img_clean, caption="Segmentation", use_container_width=True)
     with col2:
-      st.write('Yellow Masked')
-      st.image(yellow_mask, width=200)
+      st.image(img_edge, caption="Egde", use_container_width=True)
     with col3:
-      st.write('Green Masked')
-      st.image(green_mask, width=200)
+      st.image(yellow_mask, caption="Yellow Mask", use_container_width=True)
     with col4:
-      st.write('Edge Detection')
-      st.image(img_edge, width=200)
-        
-if "image" in st.session_state:
-  st.header('Uploaded Image')
-  st.write(st.session_state.image)
-  if st.button('Check Ripeness'):
-    st.header('Prediction Result')
-    with st.spinner('Loading Prediction...'):
-      predict(st.session_state.image)
+      st.image(green_mask, caption="Green Mask", use_container_width=True)
+
+    if combined_image is not None:
+      next_predict = True
+    else:
+      st.error("Classified as Not a Banana based on edge density.")
+
+  if next_predict :
+    st.markdown("<h2 style='text-align: center;'>- Predicting Ripeness -</h2>", unsafe_allow_html=True)
+    with st.spinner("Predicting..."):
+      load_model()
+      predict_img = [combined_image]
+      predict_img = np.array(predict_img, dtype=float)
+      predict_img /= 255.0
+      prediction = model.predict(predict_img)
+
+      if (prediction < 0.5):
+        st.success("Objek di atas dinyatakan sebagai pisang MENTAH dengan tingkat kematangan {:.5f}%".format(prediction[0][0]*100))
+      else:
+        st.warning("Objek di atas dinyatakan sebagai pisang MATANG dengan tingkat kematangan {:.5f}%".format(prediction[0][0]*100))
+
+    st.markdown("<h2 style='text-align: center;'>- Discrete from Each Layer -</h2>", unsafe_allow_html=True)
+    with st.spinner("Getting Each Layers' Discrete..."):
+      with st.expander("See Details"):
+        get_discrete(predict_img)
+
+
+def get_discrete(predict_img) :
+  global model2
+  feature_maps = model2.predict(predict_img)
+
+  # Hasil conv 2D layer 2
+  square = 8
+  for fmap in feature_maps[:1]:
+    ix = 1
+    fig, axes = plt.subplots(square, square, figsize=(8, 8))
+    for i in range(square):
+      for j in range(square):
+        # Access the specific subplot (i, j)
+        ax = axes[i, j]
+        ax.set_xticks([])  # Remove x-axis ticks
+        ax.set_yticks([])  # Remove y-axis ticks
+        # Plot the filter channel in grayscale (use the correct index for feature map)
+        ax.imshow(fmap[0, :, :, ix-1], cmap='gray')
+        ix += 1
+    st.markdown("<h3 style='text-align: center;'>- 2nd Layer: Convolution 2D -</h3>", unsafe_allow_html=True)
+    st.pyplot(fig)  # Display the figure in Streamlit
+
+  # Hasil conv 2D layer 4
+  square = 8
+  for fmap in feature_maps[1:2]:
+    ix = 1
+    fig, axes = plt.subplots(square, square, figsize=(8, 8))
+    for i in range(square):
+      for j in range(square):
+        # Access the specific subplot (i, j)
+        ax = axes[i, j]
+        ax.set_xticks([])  # Remove x-axis ticks
+        ax.set_yticks([])  # Remove y-axis ticks
+        # Plot the filter channel in grayscale (use the correct index for feature map)
+        ax.imshow(fmap[0, :, :, ix-1], cmap='gray')
+        ix += 1
+    st.markdown("<h3 style='text-align: center;'>- 4th Layer: Convolution 2D -</h3>", unsafe_allow_html=True)
+    st.pyplot(fig)  # Display the figure in Streamlit
+
+  # Hasil flatten layer 6 sbg input LSTM
+  flatten_map = feature_maps[2:3][0][0]
+  st.markdown("<h3 style='text-align: center;'>- 6th Layer: Flatten -</h3>", unsafe_allow_html=True)
+  st.write(np.vectorize(lambda x: f'{x:.3f}')(flatten_map).reshape(1, -1))
+
+
+# Main page
+col1, col2 = st.columns([1, 4])
+with col1:
+  st.image("https://www.shutterstock.com/image-vector/banana-relax-sunglasses-cartoon-mascot-600nw-2402786913.jpg")
+with col2:
+  st.markdown("<h1 style='text-align: center;'>Banana Ripeness Detector</h1>", unsafe_allow_html=True)
     
-# footer
+input_container = st.container(border=True)
+uploaded_file = input_container.file_uploader(
+  label="Upload a Banana Image",
+  type=["jpg","jpeg","png"],
+)
+if uploaded_file is not None:
+  st.session_state.image = uploaded_file
+  st.session_state.source = "upload"
+else :
+  # Remove uploaded image and prepare for capturing
+  if "source" in st.session_state and st.session_state.source == "upload" and "image" in st.session_state:
+      del st.session_state.image
+
+  @st.dialog("Take a Photo")
+  def capture_image():
+    st.write("Take a picture of a piece of banana. Make sure it's fully visible and not blurry.")
+    picture = st.camera_input("Take a picture")
+    if picture:
+      st.session_state.image = picture
+      st.rerun()
+
+  input_container.markdown("<p style='text-align: center;'>OR</p>", unsafe_allow_html=True)
+  take_photo = input_container.button("📸 Take a Photo", use_container_width=True)
+  if take_photo:
+    st.session_state.source = "camera"
+    capture_image()
+
+if "image" in st.session_state:
+  col1, col2, col3 = st.columns([1, 3, 1])  # Adjust column widths as needed
+  # Display images in 2nd column only
+  with col2:
+    st.image(st.session_state.image, caption="User's uploaded Banana Image" if st.session_state.source=="upload" else "User's captured Banana Image", use_container_width=True)
+  predicting = st.button("Predict Ripeness", use_container_width=True)
+  if predicting:
+    process_prediction(st.session_state.image)
+
+
 footer_html = """
-<style>
-  footer {
-    position: fixed;
-    left: 0;
-    padding: 16px;
-    bottom: 0;
-    width: 100%;
-    background-color: #FDFD96;
-    color: black;
-    text-align: center;
-  }
-</style>
-<footer style='text-align: center;'>
-  <p style='margin:0'>Copyright &copy; 2024 by (AB)CDEF: Cindy, Dhea, Erin, Farrel. All Right Reserved.</p>
-</footer>
+  <style>
+    footer {
+      position: fixed;
+      left: 0;
+      padding: 10px;
+      bottom: 0;
+      width: 100%;
+      background-color: #FDFD96;
+      color: black;
+      text-align: center;
+    }
+  </style>
+  <footer style='text-align: center;'>
+    <p style='margin:0'>Copyright &copy; 2024 by (AB)CDEF: Cindy, Dhea, Erin, Farrell. All Right Reserved.</p>
+  </footer>
 """
 st.markdown(footer_html, unsafe_allow_html=True)
--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,26 @@
-import rembg
-import numpy as np
+# Importing all lib
 from PIL import Image, ImageOps
+import numpy as np
+import rembg
 import cv2
 
+def resize_with_padding(img: Image.Image, expected_size: tuple[int,int]) -> Image.Image:
+  '''
+    Resize the Image object to the expected size (width, height); adding zero padding to fill empty pixels
+  '''
+  img.thumbnail((expected_size[0], expected_size[1]))
+  delta_width = expected_size[0] - img.size[0]
+  delta_height = expected_size[1] - img.size[1]
+  pad_width = delta_width // 2
+  pad_height = delta_height // 2
+  padding = (pad_width, pad_height, delta_width - pad_width, delta_height - pad_height)
+  return ImageOps.expand(img, padding)
+
+
 def remove_background(img: Image.Image) -> Image.Image:
-  """
+  '''
     Accept an Image object, remove the background, and returns Image with removed background
-  """
+  '''
   input_array = np.array(img)
 
   # Apply background removal using rembg
@@ -17,75 +31,33 @@ def remove_background(img: Image.Image) -> Image.Image:
 
   return output_image
 
-def resize_with_padding(img: Image.Image, expected_size: tuple[int,int]) -> Image.Image:
-  """
-    Resize the Image object to the expected size (width, height); adding zero padding to fill empty pixels
-  """
-  img.thumbnail((expected_size[0], expected_size[1]))
-  delta_width = expected_size[0] - img.size[0]
-  delta_height = expected_size[1] - img.size[1]
-  pad_width = delta_width // 2
-  pad_height = delta_height // 2
-  padding = (pad_width, pad_height, delta_width - pad_width, delta_height - pad_height)
-  return ImageOps.expand(img, padding)
 
 def create_binary_mask(img: Image) -> Image:
-  """
+  '''
     Accept an Image object, return the binary Image
-  """
+  '''
   img = np.array(img)
   gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-  blur = cv2.GaussianBlur(gray_image, (11,11), 0)
-  et,th1 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY)
+  blur = cv2.GaussianBlur(gray_image, (11, 11), 0)
+  et, th1 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY)
   # th1 = cv2.bitwise_not(th1)
 
   return Image.fromarray(th1)
 
-# Edge detection with Canny
-def edge_detection(image: Image.Image) -> list[Image.Image, float]:
-  '''
-    Accept Image object in grayscale mode, return a list containing\n
-    the edge detection Image result using Canny filter and edge strength
-  '''
-  gray_img = np.array(image)
-
-  edges = cv2.Canny(gray_img, 150, 200)  # 100, 200
-  edge_strength = np.sum(edges) / 255.0
-
-  # Stack the edges back into 3 channels to maintain shape consistency
-  edges_stacked = np.stack((edges,)*3, axis=-1)
-
-  edges_img = Image.fromarray(edges_stacked)
-
-  return edges_img, edge_strength
-
-def calculate_edge_density(edge_map: cv2.typing.MatLike, roi_mask: cv2.typing.MatLike) -> float:
-  '''
-    Calculate edge density within the region of interest (ROI)
-  '''
-
-  roi_edges = cv2.bitwise_and(edge_map, edge_map, mask=roi_mask)
-  # plt.imshow(roi_edges)
-  edge_pixels = np.sum(roi_edges) / 255  # Count edge pixels (each pixel is 255 in binary)
-  total_pixels = np.sum(roi_mask) / 255  # Count pixels in the ROI
-
-  if total_pixels == 0:  # Avoid division by zero
-    return 0
-  edge_density = edge_pixels / total_pixels
-  return edge_density
 
 def getLargestContour(img_array: cv2.typing.MatLike) -> cv2.typing.MatLike:
   '''
     Calculate the largest contour (bounding box)
   '''
   des = img_array
-  contour, hier = cv2.findContours(des,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+  contour, hier = cv2.findContours(des, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
   for cnt in contour:
-      cv2.drawContours(des,[cnt],0,255,-1)
+    cv2.drawContours(des,[cnt],0,255,-1)
 
   largest_contour = max(contour, key=cv2.contourArea)
+  # print(largest_contour.shape)
   return largest_contour
 
 def resize_to_bounding_box(image: Image.Image, largest_contour: cv2.typing.MatLike) -> Image.Image:
@@ -100,9 +72,10 @@ def resize_to_bounding_box(image: Image.Image, largest_contour: cv2.typing.MatLi
   banana_roi_resized = resize_with_padding(banana_roi, (128,128))
   return banana_roi_resized
 
-def create_mask(image_rgb: cv2.typing.MatLike, lower: list[int,int,int], upper: list[int,int,int]) -> cv2.typing.MatLike:
+
+def create_mask(image_rgb: cv2.typing.MatLike, lower: list[int, int, int], upper: list[int, int, int]) -> cv2.typing.MatLike:
   '''
-    Accept np.array-ed RGB Image, HSV color low range, HSV color upper range\n
+    Accept np.array-ed RGB Image, HSV color low range, HSV color upper range.
     Return array of masked Image
   '''
   # Ensure image_rgb is 3-channel RGB (png has 4 channel, RGBA)
@@ -110,7 +83,7 @@ def create_mask(image_rgb: cv2.typing.MatLike, lower: list[int,int,int], upper: 
     # Convert to 3-channel RGB if necessary
     image_rgb = cv2.cvtColor(image_rgb, cv2.COLOR_RGBA2RGB)
 
-  # To handle opencv manipulations, need to store in bgr bcs opencv processed images in bgr mode
+  # To handle opencv manipulations, need to store in BGR bcs opencv processed images in BGR mode
   image_bgr = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
   # Convert the image to HSV color
   image_hsv = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2HSV)
@@ -148,12 +121,67 @@ def create_green_mask(image: cv2.typing.MatLike) -> cv2.typing.MatLike:
   upper_green = np.array([75, 255, 255])
   return create_mask(image, lower_green, upper_green)
 
-def preprocess_input(img: Image.Image) -> list[cv2.typing.MatLike]:
+
+# Edge detection with Canny
+def edge_detection(image: Image.Image) -> list[Image.Image, float]:
   '''
-    Preprocess image from user.\n
-    Accepts PIL Image, returns (128,128,9) image (yellow mask, green mask, edge)
+    Accept Image object in grayscale mode, return a list containing the edge detection Image result using Canny filter and edge strength
   '''
-  img = resize_with_padding(img, (500,500))
+  gray_img = np.array(image)
+
+  edges = cv2.Canny(gray_img, 150, 200)  # 100, 200
+  edge_strength = np.sum(edges) / 255.0
+
+  # Stack the edges back into 3 channels to maintain shape consistency
+  edges_stacked = np.stack((edges,)*3, axis=-1)
+
+  edges_img = Image.fromarray(edges_stacked)
+
+  return edges_img, edge_strength
+
+
+def calculate_edge_density(edge_map: cv2.typing.MatLike, roi_mask: cv2.typing.MatLike) -> float:
+  '''
+    Calculate edge density within the Region of Interest (ROI)
+  '''
+
+  roi_edges = cv2.bitwise_and(edge_map, edge_map, mask=roi_mask)
+  # plt.imshow(roi_edges)
+  edge_pixels = np.sum(roi_edges) / 255  # Count edge pixels (each pixel is 255 in binary)
+  total_pixels = np.sum(roi_mask) / 255  # Count pixels in the ROI
+
+  if total_pixels == 0:  # Avoid division by zero
+    return 0
+  edge_density = edge_pixels / total_pixels
+  return edge_density
+
+
+def flatten_input(yellow_mask: np.ndarray, green_mask: np.ndarray, img_edge: Image.Image) -> np.ndarray:
+  '''
+    Combines three image arrays into a single multi-channel array.
+
+    Args:
+      yellow_mask: A NumPy array representing the yellow mask of the image.
+      green_mask: A NumPy array representing the green mask of the image.
+      img_edge: A NumPy array representing the edge detection result of the image.
+
+    Returns:
+      A NumPy array with the three input arrays stacked along the last axis (channel axis).
+  '''
+
+  # Flatten image to (128, 128, 9) shaped
+  flattened_image = np.dstack((yellow_mask, green_mask, img_edge))
+  return flattened_image
+
+
+# PREPROCESSING STEPS =====================================================================================
+
+def preprocess_input(img: Image.Image) -> cv2.typing.MatLike:
+  '''
+    Preprocess image from user.
+    Accepts PIL Image, returns (128, 128, 9) image (yellow mask, green mask, edge)
+  '''
+  img = resize_with_padding(img, (500, 500))
   img_clean = remove_background(img)
 
   img_binary = create_binary_mask(img_clean)
@@ -175,17 +203,13 @@ def preprocess_input(img: Image.Image) -> list[cv2.typing.MatLike]:
   edge_density = calculate_edge_density(np.array(img_edge), roi_mask) * 100
 
   # fine tuning needed
-  # edge_density_threshold_low = 15
-  # edge_density_threshold_high = 35
+  # edge_density_threshold_low = density.min() - 0.25   # 15
+  # edge_density_threshold_high = density.max() + 0.25  # 35
   edge_density_threshold_low = 5.59 - 0.25
   edge_density_threshold_high = 12.58 + 0.25
   if edge_density < edge_density_threshold_low or edge_density > edge_density_threshold_high:
-    print("Classified as Not a Banana based on edge density.")
-    return [[yellow_mask, green_mask, img_edge]]  # Skip further processing for not-a-banana
+    # print("Classified as Not a Banana based on edge density.")
+    return None, img_clean, yellow_mask, green_mask, img_edge  # Skip further processing for not-a-banana
 
-  combined_image = np.dstack((yellow_mask, green_mask, img_edge))
-  print('Density', edge_density)
-  # print(np.array(yellow_mask).max())
-  # print(np.array(green_mask).max())
-  # print(np.array(img_edge).max())
-  return [[yellow_mask, green_mask, img_edge], combined_image]
+  combined_image = flatten_input(yellow_mask, green_mask, img_edge)
+  return combined_image, img_clean, yellow_mask, green_mask, img_edge
